20c48a66fab810f0f6cda9824de16ac50a9331c9
Qin Xueying <qinxueying1995@gmail.com>
2014-10-01 21:00:52 +0800

    Add function in CommandData.h, Add CommandData.cpp

--- efficiency/EffiData/CommandData.h
+++ efficiency/EffiData/CommandData.h
@@ -1,4 +1,17 @@
 #ifndef COMMANDDATA_H
 #define COMMANDDATA_H
+#include "EventData.h"
+#include "EventData.cpp"
+#include "TaskList.h"
 
+class CommandData{
+public:
+	static boolean createTaskList(string taskListName, string userName);
+	static boolean addEvent(Event e);
+	static boolean deleteEvent(Event e);
+	static vector<string> readAll();
+	static boolean updateTaskList();
+	static string search(string name);
+	static string search(string keyWords);
+};
 #endif
