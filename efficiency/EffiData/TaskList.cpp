f6985fede75912be7e9b42a2f3c6e0b9d96d2c6a
BlacKeNinG <blackening@gmail.com>
2014-10-06 19:01:53 +0800

    [DB] TaskList.cpp implementation.

--- efficiency/EffiData/TaskList.cpp
+++ efficiency/EffiData/TaskList.cpp
@@ -1,2 +1,29 @@
 #include "TaskList.h"
 
+//TODO: test everything.
+Event::UUID TaskList::addEvent(std::string name, EventOperator op = [](Event& evt){ }){
+	Event evt(name);
+	op(evt);
+	userTaskList.insert(make_pair(evt.getId(), evt));
+}
+
+void TaskList::updateEvent(Event::UUID id, EventOperator op){
+	op(userTaskList.at(id));
+}
+
+void TaskList::deleteEvent(Event::UUID id){
+	userTaskList.erase(id);
+}
+
+Event TaskList::getEvent(Event::UUID id){
+	return Event(userTaskList.at(id));
+}
+
+vector<Event> TaskList::getAllEvents(){
+	vector<Event> results;
+	for(auto it = userTaskList.begin(); it!= userTaskList.end(); ++it)
+	{
+		results.push_back(Event(get<1>(*it)));
+	}
+	return results;
+}
--- efficiency/EffiData/TaskList.h
+++ efficiency/EffiData/TaskList.h
@@ -15,13 +15,13 @@ protected:
 	//string name;
 public:
 	//void setName(string name);
-	Event::UUID addEvent(std::string name);
+	Event::UUID addEvent(std::string name, EventOperator op);
 	//update using a functional.
 		//This enforces that all changes are done at once.
 	void updateEvent(Event::UUID id, EventOperator op); 
 	void deleteEvent(Event::UUID id);
 	Event getEvent(Event::UUID id);  //Note that this returns a copy.
-	vector<Event> getAllEvents();
+	vector<Event> getAllEvents(); //This returns copies too.
 	friend ostream& operator<<(ostream& os, const TaskList t);
 };
 
