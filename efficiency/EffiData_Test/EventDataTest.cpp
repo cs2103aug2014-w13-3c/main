063f85865ce77f027f6e4348d2821017a54588a1
BlacKeNinG <blackening@gmail.com>
2014-10-06 17:42:15 +0800

    [DB] Added test cases for id generation and printing.

--- efficiency/EffiData_Test/EventDataTest.cpp
+++ efficiency/EffiData_Test/EventDataTest.cpp
@@ -5,13 +5,55 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace EffiData_Test
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(EffiData_Test)
 	{
+	protected:
+		streambuf * oldStdoutBuf;
+		stringstream consoleOutput;
+		string consoleDump(){ 
+			std::istreambuf_iterator<char> eos;
+			std::string sout(std::istreambuf_iterator<char>(consoleOutput), eos);
+			return sout;
+		}
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		//Redirect cout.
+		TEST_METHOD_INITIALIZE(pre) {
+			oldStdoutBuf = std::cout.rdbuf();
+			std::cout.rdbuf(consoleOutput.rdbuf());
+		}
+
+		//Restore redirect cout
+		TEST_METHOD_CLEANUP(post) 
 		{
-			// TODO: Your test code here
+			std::cout.rdbuf(oldStdoutBuf);
+		}
+
+		TEST_METHOD(BasicPrintCheck)
+		{
+			Event e("test event");
+			string expected = "{\n"
+			"    \"name\": \"test event\",\n"
+			"    \"id\": \"0\",\n"
+			"    \"priority\": \"5\",\n"
+			"    \"tags\": \"\",\n"
+			"    \"complete\": \"false\",\n"
+			"    \"start\": \"0\",\n"
+			"    \"end\": \"0\",\n"
+			"    \"parent\": \"0\",\n"
+			"    \"content\": \"0\"\n"
+			"}\n";
+			std::cout<<e;
+			string result = consoleDump();
+			Logger::WriteMessage(result.c_str());
+			Assert::AreEqual(expected, result);
+
+		}
+
+		TEST_METHOD(IDIncrementCheck)
+		{
+			Event e("test event");
+			Event e2("test event");
+			Assert::AreEqual(e.getId() +1 ,e2.getId());
 		}
 
 	};
