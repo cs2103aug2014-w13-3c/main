359cc57a66b7b4a3b2052d6530075dac605d8cd7
Tan Tack poh <colonel.tan911220@gmail.com>
2014-10-20 01:40:39 +0800

    Some option field implemented, Some unit Testing
    option fields with one value (such as priority and link taskID) has been
    done.
    
    Unit testing has passed for scenarios like bad command keyword, missing
    parameters, extra field values and non-existent option fields

--- efficiency/effiCommand/commandTypeEnum.h
+++ efficiency/effiCommand/commandTypeEnum.h
@@ -5,7 +5,10 @@ namespace CommandTypeEnum {
 
 	// these are the possible command types
 	enum COMMAND_TYPE {
-		ADD_TASK, DELETE_TASK, EXIT
+		VIEW, MARK_COMPLETE,
+		ADD_TASK, UPDATE_TASK, DELETE_TASK,  
+		SEARCH, FILTER, 
+		UNDO, SETTINGS, MINIMIZE, HELP, LOGOUT, EXIT
 	};
 
 };
--- efficiency/effiCommand/optionField.h
+++ efficiency/effiCommand/optionField.h
@@ -13,10 +13,13 @@ namespace cmdOptionField {
 	static const string START_OPTION = "-s";
 	static const string END_OPTION = "-e";
 	static const string PRIORITY_OPTION = "-p";
-	static const string RECURSIVE_OPTION = "-r";
+	static const string REPEAT_OPTION = "-r";
+	static const string RECURSIVE_OPTION = "-dr";
 	static const string TAG_OPTION = "-t";
 	static const string LINK_OPTION = "-l";
 	static const string HELP_OPTION = "-h";
+
+	static const string EMPTY_FIELD = "-!";
 
 };
 
